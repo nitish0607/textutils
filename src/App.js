8997cdaa6a0cba5093ab3571519ef863605e40b9
nitishpatel <nitishpatel0607@gmail.com>
2023-06-11 16:20:19 +0530

    refix route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import AboutUs from './components/AboutUs';
 
-import React , {useState} from 'react';
+import React ,{Navigate ,useState} from 'react';
 import {
   BrowserRouter as Router,
   Route, 
-  Routes
+  Routes,
 } from "react-router-dom";
 
 
@@ -49,8 +49,10 @@ else{
 <Alert alert={alert}/>
 <div className="container my-3">
 <Routes>
+
+          <Route path='/' element={<Navigate to='/textutils' />} />
           <Route exact path="/About"element={<AboutUs/>}/>
-          <Route exact path="/"element={<TextForm heading="Enter Any Text To Analyze Below" mode={mode} showAlert={showAlert}/>}/>
+          <Route exact path="/textutils"element={<TextForm heading="Enter Any Text To Analyze Below" mode={mode} showAlert={showAlert}/>}/>
           {/* <TextForm heading="Enter Any Text To Analyze Below" Color="yellow" mode={mode} showAlert={showAlert}/> */}
         </Routes>
         </div>
